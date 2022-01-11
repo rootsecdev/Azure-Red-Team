651b9594ce6f00ba642361a6c889ce0f0ebbf6ba
rootsecdev <rootsecdev@gmail.com>
2022-01-11 13:37:04 -0600

    Add files via upload

---
+++ BannedPasswordProtection/BadPass.ps1
@@ -0,0 +1,26 @@
+﻿function Invoke-BadPass{
+
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$File,
+        [string]$user
+    )
+    
+    if (!$File) {Write-Host "Invalid file path..."; break}
+
+    $Passwords = Get-Content $File
+    $Error = $false
+    
+     ForEach ($Password in $Passwords){
+     
+     Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -ErrorAction SilentlyContinue -ErrorVariable ProcessError;
+     
+     If ($ProcessError)
+        {
+            
+            Write-Host "Changing password for: " $user : $password -ForegroundColor Red "[*] Failed!"}
+    Else{
+
+            Write-Host "Changing password for: " $user : $password -ForegroundColor Green "[*] Success!"}
+    
+}}
